f5b3601a01d60297697dea931b809cd6e7d3ca61
Guspascad420 <Paacad@gmail.com>
2024-01-18 20:40:50 +0800

    Initial commit

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"midtrans-go-api/controllers"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
 	r.POST("/api/charge", controllers.ChargeRequest)
-	r.POST("/api/otp/generate", controllers.GenerateAndSendOTP)
-	r.POST("/api/otp/validate", controllers.ValidateOTP)
 	r.GET("/api/status/:orderId", controllers.CheckOrder)
 	r.Run()
 }
